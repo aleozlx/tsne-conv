64abf111fa665a7cff2d31a459822dbb7c0aee59
Alex Yang <aleozlx@gmail.com>
2017-12-03 13:18:50 -0600

    checkpoint

--- tsne.py
+++ tsne.py
@@ -17,28 +17,24 @@ from keras.optimizers import Adam, SGD
 from keras.preprocessing.image import ImageDataGenerator
 from keras.applications.vgg16 import VGG16
 
-DATASET = lambda fname = '': os.path.join('/dsa/data/all_datasets/transfer_learning/DogsCats', fname)
+DATASET = lambda fname = '': os.path.join('B:/TransferLearning', fname)
 assert os.path.exists(DATASET())
 
 class DogsVsCats(Model):
     def __init__(self):
         self.images = Input(shape = [150, 150, 3])
         self.vgg16 = VGG16(weights = None, include_top = False)
-        
         classifier = [
             Flatten(input_shape = self.vgg16.output_shape[1:]),
             Dense(256, activation='relu'),
             Dropout(0.5),
             Dense(1, activation='sigmoid'),
         ]
-
         self.prediction = functools.reduce(lambda f1, f2: f2(f1), [self.images, self.vgg16]+classifier)
-        
         super(DogsVsCats, self).__init__(
             inputs = [self.images],
             outputs = [self.prediction]
         )
-        
         self.compile(loss='binary_crossentropy',
             optimizer=SGD(lr=1e-4, momentum=0.9),
             metrics=['accuracy'])
@@ -47,47 +43,33 @@ class DogsVsCats(Model):
         for layer in self.vgg16.layers:
             layer.trainable = trainable
 
-y_pred = functools.reduce(lambda f1, f2: f2(f1), [images, vgg16]+classifier)
+if 1:
+    model = DogsVsCats()
+    model.load_weights(DATASET('weights_dogs_cats.h5'))
 
-model = DogsVsCats()
-# model.load_weights('./weights_dogs_cats.h5')
-BATCH_SIZE = 20
+    BATCH_SIZE = 20
+    test_datagen = ImageDataGenerator(rescale = 1. / 255)
+    validation_generator = test_datagen.flow_from_directory(
+        DATASET('TransferLearning/validation'),
+        target_size=(150, 150),
+        batch_size=BATCH_SIZE,
+        class_mode='binary')
 
-# Data augmentation
-train_datagen = ImageDataGenerator(
-    rescale = 1. / 255,
-    shear_range = 0.2,
-    zoom_range = 0.2,
-    horizontal_flip = True)
+    loss, accuracy = model.evaluate_generator(validation_generator, steps = 800 // BATCH_SIZE)
+    print('loss:', loss, 'accuracy:', accuracy)
 
-test_datagen = ImageDataGenerator(rescale = 1. / 255)
+model = VGG16(weights = None, include_top = False)
+last_layer = None
+with h5py.File(DATASET('weights_dogs_cats.h5')) as f:
+    vgg16_weights = f['vgg16']
+    for name in vgg16_weights:
+        print('Loading weights for', name)
+        weights = list(map(lambda pname:np.array(vgg16_weights[name][pname]), ['kernel:0', 'bias:0']))
+        last_layer = model.get_layer(name)
+        last_layer.set_weights(weights)
 
-train_generator = train_datagen.flow_from_directory(
-    DATASET('TransferLearning/train'),
-    target_size=(150, 150),
-    batch_size=BATCH_SIZE,
-    class_mode='binary')
-
-validation_generator = test_datagen.flow_from_directory(
-    DATASET('TransferLearning/validation'),
-    target_size=(150, 150),
-    batch_size=BATCH_SIZE,
-    class_mode='binary')
-
-#model.fit_generator(
-#        train_generator, steps_per_epoch = 12500 // BATCH_SIZE,
-#        validation_data=validation_generator, validation_steps=800 // BATCH_SIZE,
-#        epochs=1)
-
-#model.save_weights('./weights_dogs_cats.h5')
-
-loss, accuracy = model.evaluate_generator(validation_generator, steps = 800 // BATCH_SIZE)
-print('loss:', loss, 'accuracy:', accuracy)
-
-#Here's how you can make a prediction for one image using this model.
-#im_test = imread(DATASET('test1/5.jpg'))
-#imshow(im_test)
-#im_test = resize(im_test, (150, 150), mode = 'reflect')
-#y_pred = new_model.predict(np.expand_dims(im_test, 0)).squeeze()
-#print(['Cat', 'Dog'][y_pred>=0.5])
-
+im_test = imread(DATASET('TransferLearning/validation/cats/14.jpg'))
+im_test = resize(im_test, (150, 150), mode = 'reflect')
+y_features = model.predict(np.expand_dims(im_test, 0))
+print('kernel_shape', last_layer.get_weights()[0].shape)
+print('feature_shape', y_features.shape)
